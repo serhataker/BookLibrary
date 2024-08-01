ee3b328a3556fe3db802585edb8faee10b51e3c9
SerhatAker <81346603+serhataker@users.noreply.github.com>
2024-08-02 00:45:21 +0300

    Add project files.

---
+++ Books.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary
+{
+    public class Books // Class: it is Includes the all attributed for the object struct
+    {
+        private string bookName;
+        private string arthurName;
+        private string arthurSurName;
+        private int bookPageNumber;
+        private string publisherHouse;
+
+         private static DateTime createDate;
+
+        public string BookName { get { return bookName; } set { bookName = value; } } // properties: controls access to the field and specifies how to calculate and return the field if it exists
+        public string ArthurName { get { return arthurName; } set { arthurName = value; } }
+        public string ArthurSurname { get { return arthurSurName; } set { arthurSurName = value; } }
+        public int BookPageNumber { get { return bookPageNumber; } set { bookPageNumber = value; } }
+        public string PublisherHouse { get { return publisherHouse; } set { publisherHouse = value; } }   
+        
+        public static DateTime CreateDate { get { return createDate; } set { createDate = value; } }
+
+
+
+      
+
+
+        static Books()// if any variable main class attributes  to be want we can use this method.
+        {
+            CreateDate = DateTime.Now;
+        }
+
+        public Books()
+        {
+           
+        }
+
+        public Books(string bookname,string arthur,string arthursr,int bookpage,string publisher)//If you want to default value assign to the properties this value return there. it is may take the parameter or not
+        {
+           BookName = bookname;
+            ArthurName = arthur;
+            BookPageNumber = bookpage;
+            PublisherHouse = publisher;
+            ArthurSurname=arthursr;
+    }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using BookLibrary;
+using System;
+namespace BookLibrariy
+{
+    class Librariy
+    { 
+    
+        static void Main(string[] args)
+        {
+
+            DateTime str=Books.CreateDate; // we use the static method it doesnt need the  instance; 
+
+            Books book1 = new Books(); // we create the instance from the object constructor method 1 (New keyword, if you instance want create the from object must the use new keyword)
+            Console.WriteLine($"Book Name:{book1.BookName} Arthur Name:{book1.ArthurName} Arthur Surname:{book1.ArthurSurname} Page Number:{book1.BookPageNumber} Publisher House:{book1.PublisherHouse} Record Create Date:{str}");   
+            Books book2 = new Books("Sefiller","Victor","Hugo",1258,"Remzi Publish House");// we create the instance from the object constructor method 2
+            Console.WriteLine($"Book name:{book2.BookName} Arthur Name:{book2.ArthurName} Arthur Surname:{book2.ArthurSurname} Page Number:{book2.BookPageNumber} Publisher House:{book2.PublisherHouse} Record Create Date:{str}");
+        }
+
+    
+    }
+
+
+}
